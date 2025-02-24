99890880bade3fcdbd4a1d7e646ded806ebee124
mmslamap <mikolas.slama1@gmail.com>
2025-02-24 09:33:03 +0100

    Added repos, services, and controllers to program.cs. Connected the app to a local ms sql DB. Added basic swagger documentation.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
+using Microsoft.EntityFrameworkCore;
 using TaskManager.Components;
+using TaskManager.Data;
+using TaskManager.Repository;
+using TaskManager.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add DbContext to the services collection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Register the TaskItemRepository with the DI container
+builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+
+// Register the TaskItemService with the DI container
+builder.Services.AddScoped<ITaskItemService, TaskItemService>();
+
+// Add services to the container
+builder.Services.AddControllers();
+
+// Register Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(7152, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+    options.ListenLocalhost(5050);
+});
 
 var app = builder.Build();
 
@@ -15,8 +45,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+app.MapControllers();
 
 
 app.UseAntiforgery();
